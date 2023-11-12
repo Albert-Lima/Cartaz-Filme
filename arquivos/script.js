ebbc2847a9a54ab6e9e8a275fbeebb53f3a9c5d0
Francisco Albert <albertsousalima@gmail.com>
2023-11-12 01:43:56 -0300

    projeto completo

--- arquivos/script.js
+++ arquivos/script.js
@@ -14,3 +14,47 @@ function abrirMenu(){
     BarraNavegação.classList.toggle('navegação2')
 }
 bttMenu.addEventListener('click', abrirMenu)
+
+var bttHome = document.querySelector('#bttHome')
+var bttFotos = document.querySelector('#bttFotos')
+var bttElenco = document.querySelector('#bttElenco')
+var elenco = document.querySelector('#elenco')
+var bttTrailer = document.querySelector('#bttTrailer')
+var Trailer = document.querySelector('#Trailer')
+var BoxAtores = document.querySelector('#BoxAtores')
+var BttMostrarElenco = document.querySelector('#RevelarElenco')
+
+function ScrollElenco(){
+    var ElencoRect = elenco.getBoundingClientRect()
+    let centery = ElencoRect.top + ElencoRect.height / 2 - window.innerHeight/2
+    window.scrollTo({
+        top: centery + window.scrollY,
+        behavior: 'smooth' 
+    })
+}
+bttElenco.addEventListener('click', ScrollElenco)
+
+function ScrollTrailer(){
+    var TrailerRect = Trailer.getBoundingClientRect()
+    let centerY = TrailerRect.top + TrailerRect.height/ 2 - window.innerHeight/2
+    window.scrollTo({
+        top: centerY + window.scrollY,
+        behavior: 'smooth'
+    })
+}
+bttTrailer.addEventListener('click', ScrollTrailer)
+
+
+function MostrarElenco(){
+    BoxAtores.style.height = 'max-content'
+    BttMostrarElenco.innerHTML = 'Mostrar Menos'
+    BttMostrarElenco.removeEventListener('click', MostrarElenco)
+    BttMostrarElenco.addEventListener('click', EsconderElenco)
+    function EsconderElenco(){
+        BoxAtores.removeAttribute('style')
+        BttMostrarElenco.removeAttribute('style')
+        BttMostrarElenco.addEventListener('click', MostrarElenco)
+        BttMostrarElenco.removeEventListener('click', EsconderElenco)
+    }
+}
+BttMostrarElenco.addEventListener('click', MostrarElenco)
